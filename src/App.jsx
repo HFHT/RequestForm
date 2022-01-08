ccbec6cc162072c2614baeb537d948e661534d8f
HFHT <admin@habitattucson.org>
2022-01-08 10:53:49 -0700

    Repairlist response message

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, forwardRef } from 'react';
-import { Button, FormGroup, FormControlLabel, Grid, Snackbar, Alert as MuiAlert, Checkbox, List, Divider, ListItem, ListItemIcon, ListItemText, ListSubheader, Switch, CircularProgress, ToggleButton, ToggleButtonGroup, Stack, Paper, Input, useMediaQuery } from '@mui/material'
-import { Task as TaskIcon, Check as CheckIcon, Cancel as CancelIcon, NotInterested as NotInterestedIcon, KeyboardReturnTwoTone } from '@mui/icons-material';
+import { Button, FormGroup, FormControlLabel, Grid, Snackbar, Alert as MuiAlert, Checkbox, List, Divider, ListItem, ListItemIcon, ListItemText, ListSubheader, Switch, CircularProgress, ToggleButton, ToggleButtonGroup, Stack, Paper, Input, useMediaQuery, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
+import { Task as TaskIcon, Check as CheckIcon, Cancel as CancelIcon, NotInterested as NotInterestedIcon, KeyboardReturnTwoTone, NoBackpackSharp } from '@mui/icons-material';
 
 import { styled } from '@mui/material/styles';
 import AutoComplete from "react-google-autocomplete";
@@ -17,7 +17,7 @@ const dbDate = () => {
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
-  padding: theme.spacing(1),
+  padding: theme.spacing(0.5),
   //  textAlign: 'center',
   color: theme.palette.text.secondary,
 }));
@@ -32,6 +32,16 @@ const isCounty = (googleAddrObj) => {
   return (result[0].long_name === "Pima County" ? 'yes' : 'no')
 }
 
+const listRepairs = (repairs) => {
+  let theList = []
+  for (const property in repairs) {
+    console.log(repairs[property])
+    if (repairs[property]) { theList.push(property) }
+  }
+  console.log(theList)
+  return (theList)
+}
+
 const isCity = (googleAddrObj, zipCodeObj) => {
   var result = googleAddrObj.address_components.filter(obj => obj.types[0] === "postal_code")
   console.log(result)
@@ -44,7 +54,7 @@ const isCity = (googleAddrObj, zipCodeObj) => {
 async function setRepairState(repairList, currentState, setter) {
   repairList.map((repair) => {
     console.log(currentState)
-    setter(currentState[repair] = false)
+    return setter(currentState[repair] = false)
   })
   return (currentState)
 }
@@ -69,7 +79,24 @@ function App(props) {
     console.log(event.target.value)
   };
   const handleRepairSel = (event, selected) => {
-    console.log(event, selected)
+    console.log(event, selected, event.target.name)
+    let newRepairs = repairs
+    console.log(newRepairs)
+    newRepairs[event.target.name] = !newRepairs[event.target.name]
+    console.log(newRepairs)
+    /* Use spread operator to make sure React detects the new state */
+    setRepairs({ ...newRepairs })
+  }
+  const handleRepairDone = (event) => {
+    console.log(event, repairs)
+    let selectedRepairs = listRepairs(repairs)
+    if (selectedRepairs.length === 0) {
+      setHasAlert(language === 'en' ? 'You must select one or more repairs!' : '¡Debe seleccionar una o más reparaciones!')
+    } else {
+      answers[whichQuestion] = "yes"
+      setAnswers(answers)
+      saveAnswer("yes")
+    }
   }
   const handleAnswer = (event, answer) => {
     console.log(event.target.value, answer, whichQuestion)
@@ -120,7 +147,7 @@ function App(props) {
         const repairSave = repairs
         repairSave[repair] = false
         console.log(repairSave)
-        setRepairs(repairSave)
+        return setRepairs(repairSave)
       })
       console.log(questions[0].RepairList)
     }
@@ -134,7 +161,7 @@ function App(props) {
         <div>
           <SelLanguage language={language} onChange={handleChange} matches={matches} />
           {questions[0].Questions[whichQuestion].action === '' &&
-            <Question question={questions[0].Questions[whichQuestion]} header={questions[0].Desc[language]} language={language} translate={yesTranslate} onChange={handleAnswer} matches={matches} />}
+            <Question question={questions[0].Questions[whichQuestion]} income={questions[0].Income} header={questions[0].Desc[language]} subHeader={questions[0].IncomeDesc[language]} language={language} translate={yesTranslate} onChange={handleAnswer} matches={matches} />}
           {questions[0].Questions[whichQuestion].action === "County" &&
             <div style={{ width: "auto" }}>
               <Item elevation={0}><h3>{language === 'en' ? 'Provide the address of the home' : 'Proporcione la dirección de la casa'}</h3></Item>
@@ -164,14 +191,14 @@ function App(props) {
             </div>
           }
           {questions[0].Questions[whichQuestion].action === "RepairList" &&
-            <RepairList repairList={questions[0].RepairList} language={language} onChange={handleRepairSel} matches={matches} />
+            <RepairList repairList={questions[0].RepairList} repairs={repairs} language={language} onChange={handleRepairSel} onClick={handleRepairDone} matches={matches} />
           }
-          <Snackbar open={hasAlert!==null} autoHideDuration={7000} onClose={handleClose}>
+          <Snackbar open={hasAlert !== null} autoHideDuration={7000} onClose={handleClose}>
             <Alert onClose={handleClose} severity="warning" sx={{ width: '100%' }}>
               {hasAlert}
             </Alert>
           </Snackbar>
-          <AnswerList answerList={answerList} language={language} />
+          <AnswerList answerList={answerList} repairs={repairs} language={language} />
         </div>
       }
     </div>
@@ -179,18 +206,22 @@ function App(props) {
 }
 export default App;
 
-const AnswerList =  ({ answerList, language }) => {
-  console.log(answerList, language)
+const AnswerList = ({ answerList, repairs, language }) => {
+  console.log(answerList, repairs, language)
+  const repairKeys = Object.keys(repairs)
+  const repairSel = repairKeys.filter((id) => {return repairs[id]})
+  console.log(repairSel)
+  const theList = [...answerList]
   return (
     <List>
-      {answerList.map((answer, i) => {
+      {theList.map((answer, i) => {
         return (
           answer[language] &&
           <ListItem key={i} sx={{ backgroundColor: '#cddbd2' }}>
             <ListItemIcon>
               <CheckIcon />
             </ListItemIcon>
-            <ListItemText primary={answer[language]} />
+            <ListItemText primary={answer[language].replace("{repairs}", repairSel.join(", "))} />
             <Divider />
           </ListItem>
         )
@@ -222,7 +253,8 @@ const SelLanguage = ({ language, onChange, matches }) => {
   )
 }
 
-const Question = ({ question, header, language, translate, onChange, matches }) => {
+const Question = ({ question, income, header, subHeader, language, translate, onChange, matches }) => {
+  console.debug(question)
   return (
     <div>
       <Item elevation={0}><h3>{header}</h3></Item>
@@ -243,31 +275,61 @@ const Question = ({ question, header, language, translate, onChange, matches }) 
           <h3>{question.q[language]}</h3>
         </Item>
       </Stack>
+      {question.attrib === "income" &&
+        <Stack direction="column" spacing={0} >
+          <Item elevation={0}><h3>{subHeader}</h3></Item>
+          <Item>
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 150 }, { maxWidth: 400 }} size="small" aria-label="a dense table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell style={{ width: "50%" }} size="small" align="center">{language === 'en' ? 'Family Size' : 'Tamaño de la familia'}</TableCell>
+                    <TableCell style={{ width: "50%" }} size="small" align="center">{language === 'en' ? 'Maximum Income' : 'Renta máxima'}</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {income.map((row, i) => (
+                    <TableRow
+                      key={i}
+                      hover={true}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell size="small" align="center">{row.size}</TableCell>
+                      <TableCell size="small" align="center">{row.maxIncome}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Item>
+        </Stack>
+      }
     </div>
   )
 }
 
-const RepairList = ({ repairList, onChange, language, matches }) => {
+const RepairList = ({ repairList, repairs, onChange, onClick, language, matches }) => {
   console.log(onChange)
+  console.log(repairs)
   return (
     <div>
-      <Button variant="contained" color="primary" endIcon={<TaskIcon />} sx={matches ? {marginLeft: '4px'} : {}}>
-      {language === 'en' ? 'Select the needed repairs -> Click when Done' : 'Seleccione las reparaciones necesarias -> Haga clic cuando esté listo'} 
+      <Button variant="contained" color="primary" onClick={onClick} endIcon={<TaskIcon />} sx={matches ? { marginLeft: '4px' } : {}}>
+        {language === 'en' ? 'Select the needed repairs -> Click when Done' : 'Seleccione las reparaciones necesarias -> Haga clic cuando esté listo'}
       </Button>
       <FormGroup>
-        <Grid container rowSpacing={1} columnSpacing={1} sx={{paddingLeft: '8px'}}>
-        {repairList.map((item, i) => {
-          return (
-            <Grid key={i} item xs={12} sm={4} md={4} lg={4} xl={4} >
-            <FormControlLabel key={i} control={
-              <Switch checked={false} onChange={onChange} name={item} />
-            }
-              label={item}
-            />
-            </Grid>
+        <Grid container rowSpacing={1} columnSpacing={1} sx={{ paddingLeft: '8px' }}>
+          {repairList.map((item, i) => {
+            return (
+              <Grid key={i} item xs={12} sm={4} md={4} lg={4} xl={4} >
+                <FormControlLabel key={i} control={
+                  <Switch checked={repairs[item]} onChange={onChange} name={item} />
+                }
+                  label={item}
+                />
+              </Grid>
             )
-        }
-        )}
+          }
+          )}
         </Grid>
       </FormGroup>
     </div>
